a3b3374840949db1783fa9406216d95f537c3d3d
Daniel Neshkoski <limun@Daniels-MacBook-Pro.local>
2021-01-10 02:18:17 +0100

    l

---
+++ build/precache-manifest.643ff88271a5d9cd62841ac1bc974bb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20548191dcf7a011af028e28fff681d",
+    "url": "/RunCmd/NewLemon/index.html"
+  },
+  {
+    "revision": "070b19402f4053985b61",
+    "url": "/RunCmd/NewLemon/static/css/main.f38965af.chunk.css"
+  },
+  {
+    "revision": "780bc0d561794b40aec8",
+    "url": "/RunCmd/NewLemon/static/js/2.49ddcd58.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/RunCmd/NewLemon/static/js/2.49ddcd58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070b19402f4053985b61",
+    "url": "/RunCmd/NewLemon/static/js/main.5a2c252c.chunk.js"
+  },
+  {
+    "revision": "2b4a6e4a7686434a5e32",
+    "url": "/RunCmd/NewLemon/static/js/runtime-main.64652a19.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RunCmd/NewLemon.git/precache-manifest.11c6aed267b401ebb7ecffb492f3fb3f.js"
+  "/RunCmd/NewLemon/precache-manifest.643ff88271a5d9cd62841ac1bc974bb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RunCmd/NewLemon.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RunCmd/NewLemon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
